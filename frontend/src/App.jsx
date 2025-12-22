66fae50387337ffdf3378f272e51aac07801401a
MusfiqueUsSalehin <musfiqueishti@gmail.com>
2025-12-23 04:57:02 +0600

    components imported

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 import { Navbar } from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Pets from './pages/Pets';
+import PetDetails from './pages/PetDetails';
+
+import MyBookings from './pages/MyBookings';
 
 export const App = () => {
-  return (
-    <div>
-      <Navbar />
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith('/owner');
 
-      Testing
-    </div>
+
+  return (
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pets' element={<Pets />} />
+        <Route path='/pets/:id' element={<PetDetails />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+      </Routes>
+
+
+
+
+    </>
   )
 }
 
